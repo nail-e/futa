91901f4140ae6cd64b3add11593e66c632f81499
Elian Rieza <elianrieza@ieee.org>
2025-06-08 22:35:32 +0400

    Update cli.py

--- src/futa/cli.py
+++ src/futa/cli.py
@@ -3,7 +3,7 @@ from futa.ai_runner import process_command
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: futa <command>")
+        print("Usage: futa \"<command>\" ")
         sys.exit(1)
 
     command = " ".join(sys.argv[1:])
